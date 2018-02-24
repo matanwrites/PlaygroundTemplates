b17d9962a0ed8e81142d9ce3fec8c350efa52ee2
Mathieu Tan <mathieu.tan@ef.com>
2018-02-24 18:39:40 +0800

    algo template

---
+++ Playgrounds/Algo.xctemplate/___FILEBASENAME___.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+
+func solution(<#arguments#>) -> <#ret#> {
+    
+    return <#ret#>
+}
+
+
+_ {
+    let input = <#input#>
+    let output = <#output#>
+    
+    print("solution is \(output)")
+}()
+
+
